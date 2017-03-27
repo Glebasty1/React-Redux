d51f72922fb91c61f24339d3244c06a857c2ff6d
Gleb Sabakarov <gleb.sabakarov@mail.ru>
2017-03-28 02:02:51 +0300

    React and Redux app

---
+++ src/app/actions/mathActions.js
@@ -0,0 +1,16 @@
+/*
+ * action creators
+ */
+export function addNumber(number) {
+    return {
+        type: "ADD",
+        payload: number
+    };
+}
+
+export function subtractNumber(number) {
+    return {
+        type: "SUBTRACT",
+        payload: number
+    };
+}
---
+++ src/app/actions/userActions.js
@@ -0,0 +1,16 @@
+/*
+ * action creators
+ */
+export function setName(name) {
+    return {
+        type: "SET_NAME",
+        payload: name
+    };
+}
+
+export function setAge(age) {
+    return {
+        type: "SET_AGE",
+        payload: age
+    };
+}
